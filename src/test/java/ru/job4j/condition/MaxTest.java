12e9a059708ac46eb0f8b13f8d61f048444729ab
alshevskiy <nikita.alshevskiy@gmail.com>
2022-10-01 16:55:06 +0300

    4.2. Тернарное сравнение [#189]

--- src/test/java/ru/job4j/condition/MaxTest.java
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -3,9 +3,7 @@ package ru.job4j.condition;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-class MaxTest {
+public class MaxTest {
 
     @Test
     public void when2To1Then2() {
