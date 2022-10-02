349bb6d9f679dae23bbb0c212e9ab05b151d5ea0
Poseydon-pc <nikita.alshevskiy@gmail.com>
2022-10-02 15:59:30 +0300

    class was refactored

--- src/test/java/ru/job4j/condition/SqAreaTest.java
+++ src/test/java/ru/job4j/condition/SqAreaTest.java
@@ -1,7 +1,8 @@
 package ru.job4j.condition;
 
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.offset;
 
 public class SqAreaTest {
 
@@ -11,7 +12,7 @@ public class SqAreaTest {
         int p = 6;
         double k = 2;
         double out = SqArea.square(p, k);
-        Assert.assertEquals(expected, out, 0.01);
+        assertThat(out).isCloseTo(expected, offset(0.01));
     }
 
     @Test
@@ -20,7 +21,7 @@ public class SqAreaTest {
         int p = 10;
         double k = 3;
         double out = SqArea.square(p, k);
-        Assert.assertEquals(expected, out, 0.01);
+        assertThat(out).isCloseTo(expected, offset(0.01));
     }
 
     @Test
@@ -29,6 +30,6 @@ public class SqAreaTest {
         int p = 120;
         double k = 5;
         double out = SqArea.square(p, k);
-        Assert.assertEquals(expected, out, 0.01);
+        assertThat(out).isCloseTo(expected, offset(0.01));
     }
 }
