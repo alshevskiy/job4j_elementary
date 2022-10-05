a315f692d01f4e5093a335e1a94891990f55b2da
alshevskiy <nikita.alshevskiy@gmail.com>
2022-10-05 19:54:16 +0300

    6.0.1. Объявление массива. [#3613]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -4,6 +4,6 @@ public class ArrayDefinition {
     public static void main(String[] args) {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
-        float[] prices = new float[49];
+        float[] prices = new float[40];
     }
 }
