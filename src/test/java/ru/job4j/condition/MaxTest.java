e9c30c8ca6fe0799d087cc6d46f46f33519d9c67
alshevskiy <nikita.alshevskiy@gmail.com>
2022-10-29 19:09:44 +0300

    3. Перегрузить метод max для трех чисел [#122644]

--- src/test/java/ru/job4j/condition/MaxTest.java
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -31,4 +31,67 @@ public class MaxTest {
         int expected = 3;
         assertThat(result).isEqualTo(expected);
     }
+
+    @Test
+    public void whenMax1To2To3Then3() {
+        int first = 1;
+        int second = 2;
+        int third = 3;
+        int result = Max.max(first, second, third);
+        int expected = 3;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenMax1To1To2Then2() {
+        int first = 1;
+        int second = 1;
+        int third = 2;
+        int result = Max.max(first, second, third);
+        int expected = 2;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenMax1To1To1ThenAny() {
+        int first = 1;
+        int second = 1;
+        int third = 1;
+        int result = Max.max(first, second, third);
+        int expected = 1;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public  void whenMax1To2To3To4Then4() {
+        int first = 1;
+        int second = 2;
+        int third = 3;
+        int fourth = 4;
+        int result = Max.max(first, second, third, fourth);
+        int expected = 4;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public  void whenMax5To5To5To6Then4() {
+        int first = 5;
+        int second = 5;
+        int third = 5;
+        int fourth = 6;
+        int result = Max.max(first, second, third, fourth);
+        int expected = 6;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public  void whenMax0To0To0To0ThenAny() {
+        int first = 0;
+        int second = 0;
+        int third = 0;
+        int fourth = 0;
+        int result = Max.max(first, second, third, fourth);
+        int expected = 0;
+        assertThat(result).isEqualTo(expected);
+    }
 }
